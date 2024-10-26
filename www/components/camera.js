d086ab0760fbc6b74df5f65836c58a3b4f5c2a43
LuchoBn <lusho99.lb@gmail.com>
2024-10-26 10:30:32 +0900

    Creación de boton de cámara, link con archivo de camara, resolucion de bug de camara

--- www/components/camera.js
+++ www/components/camera.js
@@ -1,5 +1,17 @@
-$(document).on("click", "#cam", function() {
-    function retorno() {
-    }
-        console.log(navigator.camera); 
+$(document).on("click", "#camera", function() {
+    
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
+    destinationType: Camera.DestinationType.FILE_URI,
+    correctOrientation: true,
+    saveToPhotoAlbum:true
+});
+function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
 });
